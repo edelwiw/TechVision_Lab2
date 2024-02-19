99ff0c669e8e6c17203b6126ae6fdaa1b8e305b5
Alex Ivanov <Edelwiw@gmail.com>
2024-02-19 19:01:17 +0300

    Base functions added

---
+++ main.py
@@ -0,0 +1,39 @@
+import cv2 as cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+# show the source image and transformed image
+def show_images(source, transformed, title="Image"):
+    img_src_rgb = cv2.cvtColor(source, cv2.COLOR_BGR2RGB) # normal colors to display
+    img_trf_rgb = cv2.cvtColor(transformed, cv2.COLOR_BGR2RGB) 
+
+    plt.figure(figsize=(13, 5))
+    plt.suptitle(title, fontsize=16)
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(img_src_rgb)
+    plt.title('Source Image')
+    plt.axis('off')
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(img_trf_rgb)
+    plt.title('Transformed Image')
+    plt.axis('off')
+
+    # adjust space between images
+    plt.subplots_adjust(wspace=0.3, hspace=0.3)
+    plt.subplots_adjust(left=0.05, right=0.95)
+
+
+
+def apply_matrix_transformations(img, matrix):
+    # apply the matrix to the image
+    return cv2.warpAffine(img, matrix, (img.shape[1], img.shape[0]))
+
+# open the image
+img = cv2.imread("test.jpeg")
+assert img is not None, "File could not be read"
+
+
+
+plt.show()
